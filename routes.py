cf0abe4c6bc89f292bdd512ef0d1908bf3b5153a
Antony Reis <antonyyyno@gmail.com>
2025-05-26 10:52:13 -0300

    Criando a base dos htmls

--- routes.py
+++ routes.py
@@ -4,4 +4,7 @@ from flask import render_template
 @app.route("/")
 def render_home():
     return render_template("index.html")
-    
+
+@app.route("/validadorcpf")
+def render_validarcpf():
+    return render_template("validadorcpf.html")
