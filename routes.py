f55181e036042cabb9a9d562f665e85a10e4241a
Antony-Reis <antonyyyno@gmail.com>
2025-06-18 21:59:23 -0300

    Teste

--- routes.py
+++ routes.py
@@ -97,9 +97,6 @@ def gerarsenha():
 
     caracteres = []
 
-    if len(caracteres) == 0:
-        return render_template("geradordesenha.html", senha='Selecione pelo menos um tipo de caractere.')
-
     if '1' in selecionados:
         caracteres.append([chr(i) for i in range(ord('A'), ord('Z') + 1)])
 
@@ -111,6 +108,9 @@ def gerarsenha():
     
     if '4' in selecionados:
         caracteres.append(list("!@#$%^&*()-_=+[]{}|;:,.<>?/`~"))
+
+    if len(caracteres) == 0:
+        return render_template("geradordesenha.html", senha='Selecione pelo menos um tipo de caractere.')
 
     senha = ''
     for i in range(tamanho_senha):
